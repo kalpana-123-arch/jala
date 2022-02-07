f700cea2014fcfee31531bdacacefa1ac0ff84ed
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-07 20:34:14 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,15 @@
+package jala;
+
+public class Test1 {
+	public static void main(String[] args) {
+        double num;
+        num = 10.5;
+        System.out.println(num);
+	}
+	{
+        float num;
+        num = 10.5f;
+        System.out.println(num);
+
+    }
+}
