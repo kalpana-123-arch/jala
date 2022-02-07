cc431ceec4f4d20586d0c0105aa4c428a69b57fa
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-07 20:58:35 +0530

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,10 @@
+package jala;
+
+public class Test4 {
+	public static void main(String[] args) {
+		float num1 = 4.25f;
+		int num2 =5;
+		System.out.println("The largest number of the two numbers is " + Math.max(num1,num2));
+		System.out.println("The smallest number of the two numbers is " + Math.min(num1,num2));
+	}
+}
