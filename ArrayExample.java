3020e2c40dc0b67a680ec720e2db7b1f44a1383c
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-10 21:57:19 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,18 @@
+package jala;
+
+public class ArrayExample {
+	public static void main(String[] args) {
+        int[] numbers = {4, 9, 7, 3, 2, 8}; 
+        int element = 2;
+        int index = -1;    
+        int i = 0;
+        while(i < numbers.length) {
+            if(numbers[i] == element) {
+                index = i;
+                break;
+            }
+            i++;
+        }
+        System.out.println("Index of "+element+" is : "+index);
+    }
+}
