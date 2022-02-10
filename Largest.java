2a8cd204d1c3931ccebe722b6f4e3022658baa7a
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-10 21:54:51 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,14 @@
+package jala;
+
+public class Largest {
+    public static void main(String[] args) {
+
+        int n1 = 10, n2 = 20, n3 = 30;
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
