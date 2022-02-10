9b64b038232eb66fcc8d47a30025962e4573660a
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-10 20:51:56 +0530

    Add files via upload

---
+++ JavaSample1.java
@@ -0,0 +1,9 @@
+package jala;
+
+public class JavaSample1 {
+	public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Bright IT Career");
+        }
+    }
+}
