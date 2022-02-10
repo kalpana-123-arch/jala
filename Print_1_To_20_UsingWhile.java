12e45c6072585854c160cfa5a96a3efae642af77
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-10 20:53:45 +0530

    Add files via upload

---
+++ Print_1_To_20_UsingWhile.java
@@ -0,0 +1,19 @@
+package jala;
+
+public class Print_1_To_20_UsingWhile {
+	
+		public static void main(String args[])
+		{
+			//loop counter initialisation
+			int i=1;
+
+			//print statement
+			System.out.println("Output is : ");
+			//loop to print 1 to 10.
+			while(i<=20)	
+			{
+				System.out.println(i);
+				i++;
+			}
+		}
+}
