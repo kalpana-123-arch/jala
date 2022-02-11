90be2a4165c2c2081120b00b33f491b9e0ab65df
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2022-02-11 21:37:59 +0530

    Add files via upload

---
+++ TestCustomException1.java
@@ -0,0 +1,52 @@
+package jala;
+
+
+	class InvalidAgeException  extends Exception  
+	{  
+	    /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public InvalidAgeException (String str)  
+	    {  
+	        // calling the constructor of parent Exception  
+	        super(str);  
+	    }  
+	}  
+	    
+	// class that uses custom exception InvalidAgeException  
+	public class TestCustomException1  
+	{  
+	  
+	    // method to check the age  
+	    static void validate (int age) throws InvalidAgeException{    
+	       if(age < 18){  
+	  
+	        // throw an object of user defined exception  
+	        throw new InvalidAgeException("age is not valid to vote");    
+	    }  
+	       else {   
+	        System.out.println("welcome to vote");   
+	        }   
+	     }    
+	  
+	    // main method  
+	    public static void main(String args[])  
+	    {  
+	        try  
+	        {  
+	            // calling the method   
+	            validate(13);  
+	        }  
+	        catch (InvalidAgeException ex)  
+	        {  
+	            System.out.println("Caught the exception");  
+	    
+	            // printing the message from InvalidAgeException object  
+	            System.out.println("Exception occured: " + ex);  
+	        }  
+	  
+	        System.out.println("rest of the code...");    
+	    }  
+	}  
